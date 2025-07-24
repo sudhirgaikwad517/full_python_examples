ca92f8aae86fddfd50d4757b6db24ce609f909cf
sudhirgaikwad517 <sudhirgaikwad517@gmail.com>
2025-07-25 01:04:32 +0530

    List methods

--- Python_List/ListCreation.py
+++ Python_List/ListCreation.py
@@ -2,6 +2,17 @@ list1 = [] #creating empty list
 
 list2 = [1,2,3,4,5,6] #creating list with 6 elements
 
-print(list1)
+list3 = [1,2,3,4,"Sonu",True , 4.5 , 4.00] #list can conatin multiple datatypes
 
-print(list2)
+print(list1)
+print(list2) #printing the list
+print(list3) #printing the list
+print(type(list1)) #printing the type
+
+print(list3[4],list3[5]) # accessing list element by the index number
+
+print(list3[len(list3)-4]) #accessing the element by negative index
+
+print(list3[:2]) # accessing the list element by slicing
+
+
