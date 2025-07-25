04cee2e0e848c421d13b7fbd897338086240ab4c
sudhirgaikwad517 <sudhirgaikwad517@gmail.com>
2025-07-25 23:19:27 +0530

    Searching and counting in the list

---
+++ Python_List/List_Searching_And_Counting.py
@@ -0,0 +1,21 @@
+list1 = [] #creating empty list
+
+list2 = [1,2,3,4,5,6] #creating list with 6 elements
+
+list3 = [1,2,1,1,1,1,1,1,1,3,4,"Sonu",False , 4.5 , 4.00] #list can conatin multiple datatypes
+
+# if "Sonu" in list3:
+#     print(list3)
+    
+# print (list3.index("Sonu"))
+
+# unique_items = set(list3)
+
+# Print and store results
+# count_data = []
+
+# for item in unique_items:
+#     count = list3.count(item)
+#     print(f"Item : {item} Count : {count} ")
+#     count_data.append([item, count])
+    
