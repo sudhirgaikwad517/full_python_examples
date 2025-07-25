02d9f2e2dd4a8c29def9ca5cbe21d33a5c0145a6
sudhirgaikwad517 <sudhirgaikwad517@gmail.com>
2025-07-25 23:27:05 +0530

    Adding the list sorting methods

--- Python_List/List_Searching_And_Counting.py
+++ Python_List/List_Searching_And_Counting.py
@@ -1,6 +1,6 @@
 list1 = [] #creating empty list
 
-list2 = [1,2,3,4,5,6] #creating list with 6 elements
+list2 = [1,2,3,25,35,66,4,5,6] #creating list with 6 elements
 
 list3 = [1,2,1,1,1,1,1,1,1,3,4,"Sonu",False , 4.5 , 4.00] #list can conatin multiple datatypes
 
---
+++ Python_List/Sorting_List.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 25 23:22:12 2025
+
+@author: Sudhir Gaikwad
+"""
+
+list2 = [1,2,3,25,35,66,4,5,6] #creating list with elements
+
+# list2.sort()
+
+# print(list2)
+
+# new_list = sorted(list2) #sort list without chenging the original list
+
+# print(new_list)
+
