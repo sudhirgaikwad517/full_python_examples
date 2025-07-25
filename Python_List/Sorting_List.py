38f5828c77bc7e3101d795648dcead327e5f54af
sudhirgaikwad517 <sudhirgaikwad517@gmail.com>
2025-07-25 23:51:09 +0530

    Adding the methods sorting list with sort and sorted also with lambda tuple

--- Python_List/Sorting_List.py
+++ Python_List/Sorting_List.py
@@ -15,3 +15,16 @@ list2 = [1,2,3,25,35,66,4,5,6] #creating list with elements
 
 # print(new_list)
 
+# list2.sort(reverse=True) # printing the sorted list in descending order
+
+# print(list2)
+
+# list3 = sorted(list2 , reverse = True)
+
+# print(list3)
+
+students = [("Alice", 85), ("Bob", 92), ("Charlie", 78)] # list contain the tuples
+
+# Sort by second element (score)
+students.sort(key=lambda x: x[0]) 
+print(students)  # Output: [('Charlie', 78), ('Alice', 85), ('Bob', 92)]
